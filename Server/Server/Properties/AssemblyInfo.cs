273cbe002c592041364135833bdb37a21affd406
Andreas Zweili <andreas@2li.ch>
2018-05-20 13:43:01 +0200

    Start again with the project

--- Server/Server/Properties/AssemblyInfo.cs
+++ Server/Server/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85d4c921-0b3d-4bd1-a3ce-47f99d4fba91")]
+[assembly: Guid("dfba4a49-264b-44b8-97fa-44cc2013386d")]
 
 // Version information for an assembly consists of the following four values:
 //
