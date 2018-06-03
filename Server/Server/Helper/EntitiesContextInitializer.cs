b4635fe0fda5a9ec53d89e8e8a5547ccc61da126
Andreas Zweili <andreas@2li.ch>
2018-06-03 20:15:38 +0200

    move the data generation into its own class

--- Server/Server/Helper/EntitiesContextInitializer.cs
+++ Server/Server/Helper/EntitiesContextInitializer.cs
@@ -12,35 +12,25 @@ namespace Server.Helper
     {
         protected override void Seed(Context context)
         {
-            List<string> salutationList = new List<string>(new string[]
-            {
-                "Dr.", "Frau", "Herr"
-            });
-            List<string> genderList = new List<string>(new string[]
-            {
-                "Männlich", "Weiblich"
-            });
 
-            foreach (var s in salutationList)
+            var salutations = GenerateData.CreateSalutations();
+            var genders = GenerateData.CreateGenders();
+            var doctors = GenerateData.CreateDoctors();
+
+            foreach (var s in salutations)
             {
-                Salutation newSalutation = new Salutation(s);
-                context.Salutations.Add(newSalutation);
+                context.Salutations.Add(s);
             }
-            foreach (var g in genderList)
+            foreach (var g in genders)
             {
-                Gender newGender = new Gender(g);
-                context.Genders.Add(newGender);
+                context.Genders.Add(g);
+            }
+            foreach (var d in doctors)
+            {
+                context.Doctors.Add(d);
             }
             context.SaveChanges();
-            Gender gender = context.Genders.FirstOrDefault();
-            Salutation salutation = context.Salutations.FirstOrDefault();
-            Country country = new Country("Schweiz");
-            City city = new City("Herzogenbuchsee", 3360, country);
-            Status status = new Status("Regionalarzt");
-            Doctor doctor = new Doctor("Max", "Ötker", gender,
-                salutation, "Musterstrasse 12", city, status);
-            context.Doctors.Add(doctor);
-            context.SaveChanges();
+
         }
     }
 }
---
+++ Server/Server/Helper/GenerateData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Server.Models;
+
+namespace Server.Helper
+{
+    public static class GenerateData
+    {
+        private static List<Salutation> salutations = new List<Salutation>();
+        private static List<Gender> genders = new List<Gender>();
+        private static List<Doctor> doctors = new List<Doctor>();
+
+        private static List<string> salutationList = new List<string>(new string[]
+        {
+            "Dr.", "Frau", "Herr"
+        });
+        private static List<string> genderList = new List<string>(new string[]
+        {
+            "Männlich", "Weiblich"
+        });
+        public static List<Salutation> CreateSalutations()
+        {
+            foreach (var s in salutationList)
+            {
+                salutations.Add(new Salutation(s)); 
+            }
+            return salutations;
+        }
+        public static List<Gender> CreateGenders()
+        {
+            foreach (var s in salutationList)
+            {
+                genders.Add(new Gender(s)); 
+            }
+            return genders;
+        }
+        public static List<Doctor> CreateDoctors()
+        {
+            Country country = new Country("Schweiz");
+            City city = new City("Herzogenbuchsee", 3360, country);
+            Status status = new Status("Regionalarzt");
+            Doctor doctor = new Doctor("Max", "Ötker", genders[0],
+                salutations[0], "Musterstrasse 12", city, status);
+            doctors.Add(doctor);
+            return doctors;
+        }
+    }
+}
