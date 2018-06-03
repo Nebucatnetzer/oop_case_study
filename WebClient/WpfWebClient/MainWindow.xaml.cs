ac23ad08578997a9ae634968a7fe406214cad54b
Strati <m1r0x@live.de>
2018-06-03 17:24:48 +0200

    initial commit
    adds new wpf client

---
+++ WebClient/WpfWebClient/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWebClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_NewCase(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PatientForm();
+        }
+
+        private void Button_Click_ViewDoctors(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new ViewDoctors();
+        }
+
+        private void Button_Click_ViewCases(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new ViewCases();
+        }
+
+        private void Button_Click_Home(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Home();
+        }
+    }
+}
