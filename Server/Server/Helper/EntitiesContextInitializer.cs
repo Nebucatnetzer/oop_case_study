2d1173eb2860b39015d5c0208f4b9d852eb56d2e
Andreas Zweili <andreas@2li.ch>
2018-06-03 20:18:46 +0200

    Revert "move the data generation into its own class"
    This reverts commit b4635fe0fda5a9ec53d89e8e8a5547ccc61da126.

--- Server/Server/Helper/EntitiesContextInitializer.cs
+++ Server/Server/Helper/EntitiesContextInitializer.cs
@@ -12,25 +12,35 @@ namespace Server.Helper
     {
         protected override void Seed(Context context)
         {
+            List<string> salutationList = new List<string>(new string[]
+            {
+                "Dr.", "Frau", "Herr"
+            });
+            List<string> genderList = new List<string>(new string[]
+            {
+                "Männlich", "Weiblich"
+            });
 
-            var salutations = GenerateData.CreateSalutations();
-            var genders = GenerateData.CreateGenders();
-            var doctors = GenerateData.CreateDoctors();
-
-            foreach (var s in salutations)
+            foreach (var s in salutationList)
             {
-                context.Salutations.Add(s);
+                Salutation newSalutation = new Salutation(s);
+                context.Salutations.Add(newSalutation);
             }
-            foreach (var g in genders)
+            foreach (var g in genderList)
             {
-                context.Genders.Add(g);
-            }
-            foreach (var d in doctors)
-            {
-                context.Doctors.Add(d);
+                Gender newGender = new Gender(g);
+                context.Genders.Add(newGender);
             }
             context.SaveChanges();
-
+            Gender gender = context.Genders.FirstOrDefault();
+            Salutation salutation = context.Salutations.FirstOrDefault();
+            Country country = new Country("Schweiz");
+            City city = new City("Herzogenbuchsee", 3360, country);
+            Status status = new Status("Regionalarzt");
+            Doctor doctor = new Doctor("Max", "Ötker", gender,
+                salutation, "Musterstrasse 12", city, status);
+            context.Doctors.Add(doctor);
+            context.SaveChanges();
         }
     }
 }
