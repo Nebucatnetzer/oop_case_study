49ed940705fe2cc3992209e76a7c966fe580722d
Andreas Zweili <andreas@2li.ch>
2018-06-28 23:49:00 +0200

    remove all the unused usings

--- Server/Server/Helper/EntitiesContextInitializer.cs
+++ Server/Server/Helper/EntitiesContextInitializer.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using Server.DB;
-using Server.Models;
 
 namespace Server.Helper
 {
--- Server/Server/Models/Person.cs
+++ Server/Server/Models/Person.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.ServiceModel;
 
 namespace Server.Models
 {
