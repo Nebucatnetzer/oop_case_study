d45e9d0fbb1dcccdc3b836e0dba219061f776506
Strati <m1r0x@live.de>
2018-08-07 19:26:57 +0200

    add code for random strain generation
    Adds code to create random strains from letters and userinputs
    further improvements needed

--- WebClient/WpfWebClient/Home.xaml.cs
+++ WebClient/WpfWebClient/Home.xaml.cs
@@ -24,7 +24,55 @@ namespace WpfWebClient
         public Home()
         {
             InitializeComponent();
+            
         }
 
+        private void btnRandomStrainGenerator_Click(object sender, RoutedEventArgs e)
+        {
+            // create new client connection
+            WpfWebClient.ServiceReferenceEHEC.ServiceClient client = new WpfWebClient.ServiceReferenceEHEC.ServiceClient();
+
+
+            // Get the number of words and letters per word.
+            int num_letters = int.Parse(txtNumLetters.Text);
+            int num_words = int.Parse(txtNumStrains.Text);
+
+            // Make an array of the letters we will use.
+            char[] letters = "AEIOUBRFTDAEIOU".ToCharArray();
+
+            // Make a random number generator.
+            Random rand = new Random();
+
+            // Make the words.
+            for (int i = 1; i <= num_words; i++)
+            {
+                // Make a word.
+                string word = "";
+                for (int j = 1; j <= num_letters; j++)
+                {
+                    // Pick a random number between 0 and 25
+                    // to select a letter from the letters array.
+                    int letter_num = rand.Next(0, letters.Length - 1);
+
+                    // Append the letter.
+                    word += letters[letter_num];
+                }
+
+                // Write the strains into a list
+                List<string> generatedStrains = new List<string>();
+
+                generatedStrains.Add(word);
+
+                foreach (var item in generatedStrains)
+                {
+                    Strain s = new Strain();
+                    s.Name = item;
+                    client.WriteStrain(s);
+                }
+
+            }
+
+            client.Close();
+        }   
     }
 }
